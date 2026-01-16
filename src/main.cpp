e183cdfd82662f72ddfc876f625f7d35a3a05ce4
smysn <1590477510@qq.com>
2026-01-17 00:42:15 +0800

    Feat: Implement FPS camera with mouse capture toggle (Tab) and fullscreen (F11)

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,53 @@
 #include "Shader.h"
 #include "Texture.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 unsigned int SCR_WIDTH = 800;
 unsigned int SCR_HEIGHT = 600;
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront;
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+bool firstMouse = true;
+float pitch = 0.0f;
+float yaw = -90.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+float lastX = SCR_WIDTH / 2.0f;
+float fov = 45.0f;
+
+bool isMouseCaptured  = true;
+bool isFullscreen = false;
+
+int windowedPosX = 0;
+int windowedPosY = 0;
+int windowedWidth = 800;
+int windowedHeight = 600;
+
 int main() {
     glfwInit();
+    
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Milestone 5", NULL, NULL);
     if (window == NULL) { glfwTerminate(); return -1; }
+
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { return -1; }
 
@@ -114,8 +144,12 @@ int main() {
     glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window)) {
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
+
+        processInput(window);
+
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -128,11 +162,11 @@ int main() {
         ourShader.use();
 
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         ourShader.setMat4("view", view);
 
         glm::mat4 projection = glm::mat4(1.0f);
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
         ourShader.setMat4("projection", projection);
 
         glBindVertexArray(VAO);
@@ -156,4 +190,125 @@ int main() {
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+        static bool tabPressed = false;
+
+        if(glfwGetKey(window, GLFW_KEY_TAB) == GLFW_PRESS)
+        {
+            if(!tabPressed)
+            {
+                isMouseCaptured = !isMouseCaptured;
+                if(isMouseCaptured)
+                {
+                    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+                }
+                else
+                {
+                    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+                }
+                tabPressed = true;
+            }
+        }
+        else
+        {
+            tabPressed = false; // 松开按键后，解锁
+        }
+
+        static bool f11Pressed = false;
+
+        if(glfwGetKey(window, GLFW_KEY_F11) == GLFW_PRESS)
+        {
+            if(!f11Pressed)
+            {
+                isFullscreen = !isFullscreen;
+
+                if(isFullscreen)
+                {
+                    // --- 切换到全屏 ---
+                    glfwGetWindowPos(window, &windowedPosX, &windowedPosY);
+                    glfwGetWindowSize(window, &windowedWidth, &windowedHeight);
+
+                    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+
+                    const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+
+                    glfwSetWindowMonitor(window, primaryMonitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+                }
+                else
+                {
+                    glfwSetWindowMonitor(window, NULL, windowedPosX, windowedPosY, windowedWidth, windowedHeight, 0);
+                }
+                f11Pressed = true;
+            }
+        }
+        else
+        {
+            f11Pressed = false;
+        }
+
+        float cameraSpeed = 2.5f * deltaTime;;
+        if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+        if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+        if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed; 
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+
+    if(!isMouseCaptured) return;
+
+    if(firstMouse)
+    {
+        lastY = static_cast<float>(ypos);
+        lastX = static_cast<float>(xpos);
+        firstMouse = false;
+    }
+    float yoffset = lastY - static_cast<float>(ypos);
+    float xoffset = static_cast<float>(xpos) - lastX;
+    lastY = static_cast<float>(ypos);
+    lastX = static_cast<float>(xpos);
+
+    float sensitivity = 0.05f;
+    yoffset *= sensitivity;
+    xoffset *= sensitivity;
+
+    pitch += yoffset;
+    yaw += xoffset;
+
+    if(pitch > 89.0f)
+        pitch = 89.0f;
+    if(pitch < -89.0f)  
+        pitch = -89.0f;
+
+        glm::vec3 front;
+        front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        front.y = sin(glm::radians(pitch));
+        front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    if(fov >= 1.0f && fov <= 45.0f)
+        fov -= static_cast<float>(yoffset);
+    if(fov <= 1.0f)
+        fov = 1.0f;
+    if(fov >= 45.0f)
+        fov = 45.0f;
 }
