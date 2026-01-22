8d5f5127410caf9b6c4f7856b8f97018471826f2
smysn <1590477510@qq.com>
2026-01-22 14:54:10 +0800

    Feat: Implement Model loading system using Assimp (Survival Backpack)

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <cmath>
 #include "Shader.h"
-#include "Texture.h"
 #include "Camera.h"
+#include "Mesh.h"
+#include "Model.h"
 
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
@@ -60,8 +61,7 @@ int main() {
     Shader ourShader("../src/shader.vs", "../src/shader.fs");
     Shader lightShader("../src/light.vs", "../src/light.fs");
 
-    Texture box("../src/container2.png");
-    Texture border("../src/container2_specular.png");
+    Model backpack("../extern/backpack/backpack.obj");
 
     glEnable(GL_DEPTH_TEST);
 
@@ -110,19 +110,6 @@ int main() {
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
 
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
-
     glm::vec3 pointLightPositions[] = {
         glm::vec3( 0.7f,  0.2f,  2.0f),
         glm::vec3( 2.3f, -3.3f, -4.0f),
@@ -130,45 +117,21 @@ int main() {
         glm::vec3( 0.0f,  0.0f, -3.0f)
     };
 
-    unsigned int VBO, VAO;
+    //光源
+    unsigned VBO, lightVAO;
 
-    glGenVertexArrays(1, &VAO);
+    glGenVertexArrays(1, &lightVAO);
     glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    //光源
-    unsigned lightVAO;
-
-    glGenVertexArrays(1, &lightVAO);
 
     glBindVertexArray(lightVAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
     glBindVertexArray(0);
-
-    ourShader.use();
-
-    ourShader.setInt("material.diffuse", 0);
-    ourShader.setInt("material.specular", 1);
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -181,14 +144,24 @@ int main() {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // 激活 Shader
         ourShader.use();
 
         ourShader.setVec3("viewPos", camera.Position);
         ourShader.setFloat("material.shininess", 32.0f);
 
-        box.bind(0);
-        border.bind(1);
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+        ourShader.setMat4("model", model);
+
+        glm::mat3 normalMatrix = glm::mat3(glm::transpose(glm::inverse(model)));
+        ourShader.setMat3("normalMatrix", normalMatrix);
 
         // directional light
         ourShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
@@ -239,26 +212,7 @@ int main() {
         ourShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
         ourShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
 
-        glm::mat4 projection = glm::mat4(1.0f);
-        projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMat4("projection", projection);
-        ourShader.setMat4("view", view);
-
-        glBindVertexArray(VAO);
-        for(unsigned int i = 0; i < 10; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
-
-            glm::mat3 normalMatrix = glm::mat3(glm::transpose(glm::inverse(model)));
-            ourShader.setMat3("normalMatrix", normalMatrix);
-
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+        backpack.Draw(ourShader);
 
         lightShader.use();
 
@@ -280,8 +234,7 @@ int main() {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &lightVAO);
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
